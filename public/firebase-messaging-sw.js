0f74ee416b8ff4281b245d955a9e681ed3e55f70
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-01-30 18:49:11 +0800

    test worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,33 @@
 
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyDqUbUiCcRilLq8mJfo3p3csfUx9o6FN7E",
+    authDomain: "canteen-e81e1.firebaseapp.com",
+    projectId: "canteen-e81e1",
+    storageBucket: "canteen-e81e1.appspot.com",
+    messagingSenderId: "345832195402",
+    appId: "1:345832195402:web:308ae5c28cfb3accb8adb3",
+    measurementId: "G-D1C80TS1FY"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
--- worker/index.js
+++ worker/index.js
@@ -1,62 +1,64 @@
-// self.addEventListener('push', function (event) {
-//     const data = JSON.parse(event.data.text())
-//     event.waitUntil(
-//       registration.showNotification(data.title, {
-//         body: data.message,
-//         icon: '/icons/icon-192x192.png'
-//       })
-//     )
-//   })
+// // self.addEventListener('push', function (event) {
+// //     const data = JSON.parse(event.data.text())
+// //     event.waitUntil(
+// //       registration.showNotification(data.title, {
+// //         body: data.message,
+// //         icon: '/icons/icon-192x192.png'
+// //       })
+// //     )
+// //   })
   
-//   self.addEventListener('notificationclick', function (event) {
-//     event.notification.close()
-//     event.waitUntil(
-//       clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
-//         if (clientList.length > 0) {
-//           let client = clientList[0]
-//           for (let i = 0; i < clientList.length; i++) {
-//             if (clientList[i].focused) {
-//               client = clientList[i]
-//             }
-//           }
-//           return client.focus()
-//         }
-//         return clients.openWindow('/')
-//       })
-//     )
-//   })
+// //   self.addEventListener('notificationclick', function (event) {
+// //     event.notification.close()
+// //     event.waitUntil(
+// //       clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
+// //         if (clientList.length > 0) {
+// //           let client = clientList[0]
+// //           for (let i = 0; i < clientList.length; i++) {
+// //             if (clientList[i].focused) {
+// //               client = clientList[i]
+// //             }
+// //           }
+// //           return client.focus()
+// //         }
+// //         return clients.openWindow('/')
+// //       })
+// //     )
+// //   })
 
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+// import { initializeApp } from 'firebase/app';
+// import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDqUbUiCcRilLq8mJfo3p3csfUx9o6FN7E",
-    authDomain: "canteen-e81e1.firebaseapp.com",
-    projectId: "canteen-e81e1",
-    storageBucket: "canteen-e81e1.appspot.com",
-    messagingSenderId: "345832195402",
-    appId: "1:345832195402:web:308ae5c28cfb3accb8adb3",
-    measurementId: "G-D1C80TS1FY"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDqUbUiCcRilLq8mJfo3p3csfUx9o6FN7E",
+//     authDomain: "canteen-e81e1.firebaseapp.com",
+//     projectId: "canteen-e81e1",
+//     storageBucket: "canteen-e81e1.appspot.com",
+//     messagingSenderId: "345832195402",
+//     appId: "1:345832195402:web:308ae5c28cfb3accb8adb3",
+//     measurementId: "G-D1C80TS1FY"
+//   };
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// const app = initializeApp(firebaseConfig);
+// const messaging = getMessaging(app);
 
-self.addEventListener('push', (event) => {
-    console.log('[firebase-messaging-sw.js] Push received. ', event);
+// self.addEventListener('push', (event) => {
+//     console.log('[firebase-messaging-sw.js] Push received. ', event);
 
-    onBackgroundMessage(messaging, (payload) => {
-        console.log('[firebase-messaging-sw.js] Received background message ', payload);
-        // Customize notification here
-        const notificationTitle = 'Background Message Title';
-        const notificationOptions = {
-            body: 'Background Message body.',
-            icon: '/icons/icon-192x192.png'
-        };
+//     onBackgroundMessage(messaging, (payload) => {
+//         console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//         // Customize notification here
+//         const notificationTitle = 'Background Message Title';
+//         const notificationOptions = {
+//             body: 'Background Message body.',
+//             icon: '/icons/icon-192x192.png'
+//         };
 
-        //waituntil 
-        //return event.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions))
-    });
-});
+//         //waituntil 
+//         return event.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions))
+//     });
+//     return event.waitUntil(self.registration.showNotification("notificationTitle", {body: "notificationOptions"}))
+        
+// });
 
-// [END messaging_on_background_message_modular]
+// // [END messaging_on_background_message_modular]
