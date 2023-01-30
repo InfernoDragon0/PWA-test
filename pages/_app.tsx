a1be757cb7aa4008b851a699bd34d727c5233dcf
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-01-30 16:31:36 +0800

    test worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from "firebase/messaging";
+
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
