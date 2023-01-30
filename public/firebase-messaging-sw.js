a8906868790e86370e0b1688c9580172397f712f
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-01-30 18:58:28 +0800

    test worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,33 +1,33 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+// import { initializeApp } from "firebase/app";
+// import { getMessaging } from "firebase/messaging/sw";
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDqUbUiCcRilLq8mJfo3p3csfUx9o6FN7E",
-    authDomain: "canteen-e81e1.firebaseapp.com",
-    projectId: "canteen-e81e1",
-    storageBucket: "canteen-e81e1.appspot.com",
-    messagingSenderId: "345832195402",
-    appId: "1:345832195402:web:308ae5c28cfb3accb8adb3",
-    measurementId: "G-D1C80TS1FY"
-});
+// // Initialize the Firebase app in the service worker by passing in
+// // your app's Firebase config object.
+// // https://firebase.google.com/docs/web/setup#config-object
+// const firebaseApp = initializeApp({
+//     apiKey: "AIzaSyDqUbUiCcRilLq8mJfo3p3csfUx9o6FN7E",
+//     authDomain: "canteen-e81e1.firebaseapp.com",
+//     projectId: "canteen-e81e1",
+//     storageBucket: "canteen-e81e1.appspot.com",
+//     messagingSenderId: "345832195402",
+//     appId: "1:345832195402:web:308ae5c28cfb3accb8adb3",
+//     measurementId: "G-D1C80TS1FY"
+// });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = getMessaging(firebaseApp);
+// // Retrieve an instance of Firebase Messaging so that it can handle background
+// // messages.
+// const messaging = getMessaging(firebaseApp);
 
-onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/firebase-logo.png'
-    };
+// onBackgroundMessage(messaging, (payload) => {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     // Customize notification here
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//       body: 'Background Message body.',
+//       icon: '/icons/icon-192x192.png'
+//     };
   
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
-  });
+//     self.registration.showNotification(notificationTitle,
+//       notificationOptions);
+//   });
   
