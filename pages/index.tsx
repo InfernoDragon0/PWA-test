ec0882b8fc2003ea1d3f17bcbf5543e93f699423
InfernoDragon0 <mushroomapplepiele@gmail.com>
2023-01-30 15:44:34 +0800

    test worker

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [installPromptEvent, setInstallPromptEvent] = useState(null);
+  const [isSubscribed, setIsSubscribed] = useState(false)
+  const [subscription, setSubscription] = useState(null)
+  const [registration, setRegistration] = useState(null)
 
   useEffect(() => {
     const handleBeforeInstallPrompt = (event: any) => {
@@ -16,6 +19,21 @@ export default function Home() {
       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== undefined) {
+      // run only in browser
+      navigator.serviceWorker.ready.then(reg => {
+        reg.pushManager.getSubscription().then(sub => {
+          if (sub && !(sub.expirationTime && Date.now() > sub.expirationTime - 5 * 60 * 1000)) {
+            setSubscription(sub as any)
+            setIsSubscribed(true)
+          }
+        })
+        setRegistration(reg as any)
+      })
+    }
+  }, [])
 
   const installApp = () => {
     if (!installPromptEvent) return;
@@ -39,6 +57,50 @@ export default function Home() {
     });
   };
 
+  const base64ToUint8Array = (base64:any) => {
+    const padding = '='.repeat((4 - (base64.length % 4)) % 4)
+    const b64 = (base64 + padding).replace(/-/g, '+').replace(/_/g, '/')
+  
+    const rawData = window.atob(b64)
+    const outputArray = new Uint8Array(rawData.length)
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+  }
+
+  const subscribeButtonOnClick = async (event: any) => {
+    event.preventDefault()
+    const sub = await (registration as any).pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: base64ToUint8Array(process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY)
+    })
+    // TODO: you should call your API to save subscription data on server in order to send web push notification from server
+    setSubscription(sub)
+    setIsSubscribed(true)
+    console.log('web push subscribed!')
+    console.log(sub)
+  }
+
+  const sendNotificationButtonOnClick = async (event:any) => {
+    event.preventDefault()
+    if (subscription == null) {
+      console.error('web push not subscribed')
+      return
+    }
+
+    await fetch('/api/notification', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        subscription
+      })
+    })
+  }
+
   const randomNotification = () => {
     const notif = Math.floor(Math.random() * 5) + 1;
     const title = 'Notification ' + notif;
@@ -59,6 +121,8 @@ export default function Home() {
         </h1>
         <button onClick={installApp}>Install App</button>
         <button onClick={notificationApp}>Accept Agreement to Notifications</button>
+        <button onClick={subscribeButtonOnClick}>Subscribe to Web Push</button>
+        <button onClick={sendNotificationButtonOnClick}>Send Web Push Notification</button>
 
         <p className={styles.description}>
           Get started by editing{' '}
